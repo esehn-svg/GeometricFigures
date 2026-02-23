ee5b008533a95c0fd3f7bb77adc7417abbaa5138
Ella Sehn <esehn@lcs.on.ca>
2026-02-23 00:57:45 -0500

    Created groups for model and view layers of the app.

--- GeometricFigures/GeometricFiguresApp.swift
+++ GeometricFigures/GeometricFiguresApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct GeometricFiguresApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CircleView()
         }
     }
+}
+#Preview {
+    CircleView()
 }
---
+++ GeometricFigures/Views/CircleView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleView.swift
+//  GeometricFigures
+//
+//  Created by Ella Sehn on 2026-02-22.
+//
+
+import SwiftUI
+
+struct CircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleView()
+}
