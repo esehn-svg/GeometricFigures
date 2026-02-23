7da0f27bec51d83f10585f96c3746f5cb90d1184
Ella Sehn <esehn@lcs.on.ca>
2026-02-23 01:11:09 -0500

    Added basic UI to control radius

--- GeometricFigures/Views/CircleView.swift
+++ GeometricFigures/Views/CircleView.swift
@@ -8,11 +8,28 @@
 import SwiftUI
 
 struct CircleView: View {
+    
+    @State var currentCircle = Circle(radius: 10)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack {
+                // TODO: Add image later
+                
+                // Label for the slider
+                Text("Radius")
+                // Slider for radius input
+                Slider(
+                    value: $currentCircle.radius,
+                    in: 1...100,
+                    step: 1.0
+                )
+                
+                // Label showing the current slider value
+                Text("Radius is \(currentCircle.radius.formatted())")
+                
+            }
+        }
     }
-}
-
-#Preview {
-    CircleView()
+    
 }
