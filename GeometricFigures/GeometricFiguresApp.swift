c4f8ae05132d12dc410099446b885a431e53f00f
Ella Sehn <esehn@lcs.on.ca>
2026-02-22 21:50:42 -0500

    Initial Commit

---
+++ GeometricFigures/GeometricFiguresApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricFiguresApp.swift
+//  GeometricFigures
+//
+//  Created by Ella Sehn on 2026-02-22.
+//
+
+import SwiftUI
+
+@main
+struct GeometricFiguresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
